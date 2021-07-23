79271dad2b4911f8b2236f5b91f96a330802af45
Default Name <Default Email>
2021-07-23 11:40:19 -0400

    catalog and common

---
+++ MicroserviceArchitecture/Microservices.Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Common
+{
+    public class Constants
+    {
+        public const string GetMongoConnectionString = "DatabaseSettings:ConnectionString";
+        public const string GetMongoDatabaseName = "DatabaseSettings:DatabaseName";
+        public const string GetMongoCollectionName = "DatabaseSettings:CollectionName";
+    }
+}
---
+++ MicroserviceArchitecture/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Microservices.Common;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>(Constants.GetMongoConnectionString));
+            var database = client.GetDatabase(configuration.GetValue<string>(Constants.GetMongoDatabaseName));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>(Constants.GetMongoCollectionName));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
