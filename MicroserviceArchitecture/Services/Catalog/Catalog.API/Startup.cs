a9a52ca2046f03cdce7e2ea223300022a867f745
Default Name <lateefrasheed10@gmail.com>
2021-07-26 19:24:59 -0400

    swagger startup update and yaml

--- MicroserviceArchitecture/Services/Catalog/Catalog.API/Startup.cs
+++ MicroserviceArchitecture/Services/Catalog/Catalog.API/Startup.cs
@@ -42,12 +42,10 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+
 
             app.UseRouting();
 
--- MicroserviceArchitecture/Services/Discount/Discount.API/Startup.cs
+++ MicroserviceArchitecture/Services/Discount/Discount.API/Startup.cs
@@ -36,12 +36,10 @@ namespace Discount.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+
 
             app.UseRouting();
 
