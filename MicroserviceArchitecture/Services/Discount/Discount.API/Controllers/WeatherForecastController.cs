ad20a2d25844fb43744daf7f39f88f0d860809fa
Default Name <Default Email>
2021-07-26 11:06:14 -0400

    discountpod yaml

--- MicroserviceArchitecture/Services/Discount/Discount.API/Controllers/WeatherForecastController.cs
+++ MicroserviceArchitecture/Services/Discount/Discount.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Discount.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
